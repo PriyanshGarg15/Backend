77462e307b6c76963a6c97f308d8c068d783a7aa
PriyanshGarg15 <116974262+PriyanshGarg15@users.noreply.github.com>
2024-01-09 18:43:47 +0530

    Add files via upload

---
+++ A_ Introduction to JS/typeof.js
@@ -0,0 +1,7 @@
+console.log(typeof 12);
+console.log(typeof "abcd");
+console.log(typeof -0);
+console.log(typeof undefined);
+console.log(typeof null); // corner case (it gives object)
+console.log(typeof {"sanket": 12});
+console.log(typeof NaN); //it is uded to tell not  a number
